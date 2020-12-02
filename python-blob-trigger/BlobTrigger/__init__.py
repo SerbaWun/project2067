4e8560ce67a5552d171240d91eef84c3e7fa1422
arbalisi12 <arbalisi12@users.noreply.github.com>
2020-12-02 13:59:18 -0500

    azure container

--- python-blob-trigger/BlobTrigger/__init__.py
+++ python-blob-trigger/BlobTrigger/__init__.py
@@ -7,3 +7,6 @@ def main(myblob: func.InputStream):
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
+
+def xmain(inMessage: func.QueueMessage, outMessage: func.Out[func.QueueMessage])
+    logging.info(f"Python blob trigger function processed blob {inMessage}\n")
